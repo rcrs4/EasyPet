9baf93b2fe948967f3ad05a90992e81f3eed9f68
Pedro Henrique <phos@cin.ufpe.br>
2021-05-04 17:11:56 -0300

    Segundo cenário funcionando

--- tests-acceptance/features/HistoricoConsulta.feature
+++ tests-acceptance/features/HistoricoConsulta.feature
@@ -1,9 +1,16 @@
 Feature: Histórico de Consulta
   Consultar o histórico de consulta realizada para o pets
 
-Scenario: Lista de pets
-  Given estou na pagina de "historico"
-  And tenho os pets "zeze" e "spike" cadastrados
-  When nao faco nada
-  Then devo ver o titulo "Historico de consultas"
-  And devo ver em lista botoes de acesso aos detalhes de "zeze" e "spike"
+  Scenario: Lista de pets
+    Given estou na pagina de "historico"
+    And tenho os pets "zeze" e "spike" cadastrados
+    When nao faco nada
+    Then devo ver o titulo "Historico de consultas"
+    And devo ver em lista botoes de acesso aos detalhes de "zeze" e "spike"
+
+  Scenario: Requisição de consultas do pet
+    Given estou na pagina de "historico"
+    And "zeze" tem consultas registrada no dia "20/04/2021" e "21/04/2021"
+    When acesso os detalhes de "zeze"
+    Then o sistema retorna uma lista de consultas de "zeze" com aa datas "20/04/2021" e "21/04/2021"
+    And sou redirecionado para "historico/pets/zeze"
--- tests-acceptance/stepdefinitions/historico.ts
+++ tests-acceptance/stepdefinitions/historico.ts
@@ -7,6 +7,7 @@ let expect = chai.expect;
 import request = require("request-promise");
 
 browser.baseUrl = "http://localhost:4200/";
+const serverBaseUrl = "http://localhost:3333/";
 
 const navigateTo = (secao: string = "") => {
   return browser.get(browser.baseUrl + "/" + secao) as Promise<any>;
@@ -22,7 +23,19 @@ defineSupportCode(function ({ Given, When, Then }) {
     async (pet1: string, pet2: string) => {}
   );
 
+  Given(
+    "{stringInDoubleQuote} tem consultas registrada no dia {stringInDoubleQuote} e {stringInDoubleQuote}",
+    (pet: string, data1: string, data2: string) => {}
+  );
+
   When("nao faco nada", () => {});
+
+  When(
+    "acesso os detalhes de {stringInDoubleQuotes}",
+    async (petSelecionado: string) => {
+      await element(by.id(petSelecionado)).click();
+    }
+  );
 
   Then("devo ver o titulo {stringInDoubleQuotes}", async (title: string) => {
     await expect(element(by.id("pageTitle")).getText()).to.eventually.equal(
@@ -35,11 +48,29 @@ defineSupportCode(function ({ Given, When, Then }) {
     async (pet1: string, pet2: string) => {
       await element
         .all(by.css("button"))
-        .map((element) => element.getAttribute('id'))
+        .map((element) => element.getAttribute("id"))
         .then((listaPets) => {
           expect(listaPets[0]).to.equal(pet1);
           expect(listaPets[1]).to.equal(pet2);
         });
     }
   );
+
+  Then(
+    "o sistema retorna uma lista de consultas de {stringInDoubleQuotes} com aa datas {stringInDoubleQuotes} e {stringInDoubleQuotes}",
+    async (petName: string, data1: string, data2: string) => {
+      await request
+        .get(serverBaseUrl + "historico/pets/" + petName)
+        .then((consultList) => {
+          expect(
+            consultList.includes('{"data":"' + data1 + '"') &&
+              consultList.includes('{"data":"' + data2 + '"')
+          ).to.equal(true);
+        });
+    }
+  );
+
+  Then("sou redirecionado para {stringInDoubleQuote}", async (newUrl: string) => {
+    navigateTo(newUrl);
+  });
 });
